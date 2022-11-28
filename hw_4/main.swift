b1e8c4059f9daa11ba03d1e533459ce5eebaf699
Bektur250605 <118821464+Bektur250605@users.noreply.github.com>
2022-11-28 19:20:44 +0600

    Update main.swift

--- hw_4/main.swift
+++ hw_4/main.swift
@@ -1,11 +1,44 @@
-//
-//  main.swift
-//  hw_4
-//
-//  Created by Бектур Каримов on 28/11/22.
-//
 
 import Foundation
 
-print("Hello, World!")
 
+var num1: String = "кратные2:"
+var num2:String = "кратные3:"
+var num3:String = "кратные4:"
+var num4:String = "кратные5:"
+for i in 0...100{
+   if i % 2 == 0{
+       num1 += "\(i),"
+print(num1)
+   }
+   if i % 3 == 0{
+       num2 += "\(i),"
+print(num2)
+   }
+   if i % 4 == 0{
+       num3 += "\(i),"
+print(num3)
+   }
+   if i % 5 == 0{
+       num4 += "\(i),"
+print(num4)
+   }
+}
+
+var word = " Яблоко,Телефон,Город,Нурдин,Футбол,Человек,Наушники,Морковь,Шкаф,Шапка,курсы,Машина,Самолет,Лампочка,Айфон,Cумка,Дверь,Окно,Книга,Работа."
+var name = ""
+var name2 = ""
+
+func bektur(){
+    for i in word{
+        if i != "," && i != "\n" && i != "."{
+            name += "\(i)"
+        }else{
+            name2 += name
+            name2 += (" \(name.count) букв\n ")
+            name  = ""
+        }
+    }
+}
+bektur()
+print(name2)
